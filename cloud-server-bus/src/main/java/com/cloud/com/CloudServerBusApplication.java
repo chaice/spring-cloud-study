161b6f48bc7d9bb0ea7bbc4a3a3b888397c4161a
ce.chai <ce.chai@7x-networks.com>
2020-05-15 18:44:51 +0800

    spring cloud

---
+++ cloud-server-bus/src/main/java/com/cloud/com/CloudServerBusApplication.java
@@ -0,0 +1,16 @@
+package com.cloud.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigServer
+public class CloudServerBusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudServerBusApplication.class, args);
+    }
+}
